545c56a245d80314b8f447513df2121b16dc1973
elvinos <aep.charles@gmail.com>
2017-02-27 21:05:33 +0000

    Created Mean Weekly Calculator
    Finished day1 27/02/2016

--- rowsimport.m
+++ rowsimport.m
@@ -3,6 +3,14 @@ clc
 clear all
 close all
 
+
+%% Variables
+
+plsct = 1; %Select the number of Plots to Create
+
+rateR=24.41;
+rateA=0.287;
+rateG=0.161;
 %% Create Data
 
 fileName = 'senateEDD.csv';
@@ -14,6 +22,8 @@ DataMat = cell2mat(Data(2:rowsData,4:colsData));
 rowsDataMat = size(DataMat,1);
 colsDataMat = size(DataMat,2);
 
+
+%% Fit Plot
 for c = 0.5:0.5:24
     cc= c*2;
     Time(:,cc)= c;
@@ -41,13 +51,30 @@ for m = 36:48
   else
       Fit(n,m) = DataMat(n,m);
   end
+ end
 end
+
+%% Costs
+
+for n=1:rowsDataMat
+    for c = 1:48
+        if 17 <= Time(1,c) <= 19
+            rate = rateR;
+        elseif 7.5 <= Time(1,c) < 17 || 19 < Time(1,c) <= 21.5
+            rate = rateA;
+        else
+            rate = rateG;
+        end
+    charge(n,c) = DataMat(n,c)*rate;
+    end
+    Daycharge(n,:)= sum(charge(n,:))./100; %Daily Cost in Pounds
 end
+
+Yearcharge = sum(Daycharge);
+
 
 %% Do Some Plotting
-plsct= 1; % Selects Data Set
-
-for plsct=1:5
+for plsct=1:1
     figure(plsct)
     maxData1= max(DataMat(plsct,:)+1);
 yyaxis left
@@ -70,20 +97,40 @@ end
 % Batterysize = Oldsize- Newsize;
 
 
-%% Intergrate
+%% Mean Power Useage
 sat = 0;
 sun = 0;
+count = 0;
+week = 1;
+weekarray(week,:)= week;
+sz=zeros(52,5);
+
 for n=1:rowsDataMat
     if  rem((n+6)/7,1) == 0 % Finds Sundays
-        disp(n)
         sun=sun+1;
     elseif rem((n)/7,1) == 0 % Finds Saturdays
-        disp(n)
         sat=sat+1;
     else
+        count = count+ 1;
+        if  count < 6
+            sz(week,count)=sum(DataMat(n,:));
+            if count == 5
+            sz(week,6)= mean(sz(week,:));
+            end
+        else
+            week = week + 1;
+            weekarray(week,:)= week;
+            count = 1;
+            sz(week,count)=sum(DataMat(n,:));
+        end
+
     end
 end
-for n=2:6 % Week1
-    sz(n-1)=trapz(Time,DataMat(n,:));
-    meanwk1=mean(sz);
-end
+figure(plsct+1);
+plot(weekarray, sz(1:52,6))
+title('Mean Weekly Power Usage Across Senate House')
+xlabel('Week')
+ylabel('Mean Weekly Power Useage /KwH')
+xlim([1,52])
+
+
