810439bf9417c6bed574c21dd9f34bd9c174e271
elvinos <aep.charles@gmail.com>
2017-02-28 20:28:35 +0000

    Created Costs Script

--- rowsimport.m
+++ rowsimport.m
@@ -3,31 +3,32 @@ clc
 clear all
 close all
 
-
 %% Variables
-
 plsct = 1; %Select the number of Plots to Create
 
+% Rates in Pence
+UnitRate = 6.832;
 rateR=24.41;
 rateA=0.287;
 rateG=0.161;
+
 %% Create Data
 
 fileName = 'senateEDD.csv';
 
 Data=senateEDD1(fileName);
-rowsData= size(Data,1)-1;
+rowsData= size(Data,1);
 colsData= size(Data,2);
 DataMat = cell2mat(Data(2:rowsData,4:colsData));
 rowsDataMat = size(DataMat,1);
 colsDataMat = size(DataMat,2);
-
-
-%% Fit Plot
 for c = 0.5:0.5:24
     cc= c*2;
     Time(:,cc)= c;
 end
+
+%% Fit Plot
+
 for n=1:rowsDataMat
 for m = 1:16
   if DataMat(n,m) > 10.5 
@@ -58,18 +59,21 @@ end
 
 for n=1:rowsDataMat
     for c = 1:48
-        if 17 <= Time(1,c) <= 19
-            rate = rateR;
-        elseif 7.5 <= Time(1,c) < 17 || 19 < Time(1,c) <= 21.5
-            rate = rateA;
+        if 17 < Time(1,c) <= 19
+            DUoSrate = rateR;
+            Rtot(n,c)= DataMat(n,c)*rateR;
+        elseif 7.5 <= Time(1,c) <= 17 || 19 < Time(1,c) <= 21.5
+            DUoSrate = rateA;
+            Atot(n,c)= DataMat(n,c)*rateA;
         else
-            rate = rateG;
+            DUoSrate = rateG;
+            Gtot(n,c)= DataMat(n,c)*rateG;
         end
-    charge(n,c) = DataMat(n,c)*rate;
+    HHcharge(n,c) = DataMat(n,c)*(DUoSrate+UnitRate);
     end
-    Daycharge(n,:)= sum(charge(n,:))./100; %Daily Cost in Pounds
+    Daycharge(n,:)= sum(HHcharge(n,:))./100; %Daily Cost in Pounds
 end
-
+TotUnit= sum(sum(DataMat));
 Yearcharge = sum(Daycharge);
 
 
